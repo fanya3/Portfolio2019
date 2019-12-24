3906a6c02de45c98f2f0cf33fda96d77facbeafa
Fanny <fanny.bouffier@gmail.com>
2019-12-24 01:49:08 +0100

    feat(navbar):added removable navbar for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,34 @@ import LogoHorizontal  from "../assets/icons/LogoFBHorizontal.svg";
 import "../_GlobalStyle.scss";
 import "../assets/css/Navbar.scss"
 
+
+const body = document.body;
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll == 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+
+  
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
+
+
+
 const Navbar = (props) => {
     return (
       <>
